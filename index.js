21aacdcb2bc030669a2573a3a28d52ac8b305d64
Steve Worley <steven.worley@acquia.com>
2016-07-28 11:43:10 +1000

    Browser support
    - Adds browser support for the tool
    - Edit and modify content prior to downloading a CSV directly in the browser

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
  * Entry point in the scraper app.
  */
 
+var express = require('express');
 var Xray = require('x-ray');
 var json2csv = require('json2csv');
 var fs = require('fs-extra');
@@ -34,8 +35,34 @@ var App = {
           this.error(confs[index] + ' missing required option: ' + options[i]);
         }
       }
-      this.scrape(conf);
+      this.scrape(conf, this.writeFile);
     }
+  },
+
+  writeFile: function(opts, result) {
+    var fields = opts.fields ? opts.fields : (typeof opts.output.length == 'undefined' ? Object.keys(opts.output) : Object.keys(opts.output[0]));
+    var exists = true;
+
+    var csv = json2csv({
+      data: result,
+      fields: fields
+    });
+
+    try {
+      fs.statSync(opts.file);
+    } catch(e) {
+      exists = false;
+    }
+
+    if (exists) {
+      csv = csv.substring(csv.indexOf("\n") + 1);
+      fs.appendFile(opts.file, "\n" + csv, this.error);
+    } else {
+      fs.ensureFile(opts.file, function(error) {
+        this.error(error);
+        fs.writeFile(opts.file, csv, this.error);
+      }.bind(this))
+    };
   },
 
   error: function(err) {
@@ -45,7 +72,7 @@ var App = {
     }
   },
 
-  scrape: function(opts) {
+  scrape: function(opts, callback) {
     this.scraper(opts.url, opts.selector, opts.output)(function(err, result) {
       if (err) {
         this.error(err);
@@ -60,31 +87,77 @@ var App = {
         return;
       }
 
-      var fields = opts.fields ? opts.fields : (typeof opts.output.length == 'undefined' ? Object.keys(opts.output) : Object.keys(opts.output[0]));
-      var exists = true;
+      callback.apply(this, [opts, result]);
 
-      var csv = json2csv({
-        data: result,
-        fields: fields
-      });
-
-      try {
-        fs.statSync(opts.file);
-      } catch(e) {
-        exists = false;
-      }
-
-      if (exists) {
-        csv = csv.substring(csv.indexOf("\n") + 1);
-        fs.appendFile(opts.file, "\n" + csv, this.error);
-      } else {
-        fs.ensureFile(opts.file, function(error) {
-          this.error(error);
-          fs.writeFile(opts.file, csv, this.error);
-        }.bind(this))
-      }
     }.bind(this));
   }
 };
 
-App.run(argv);
+if (argv.browser) {
+  var server = express();
+  var glob = require('glob');
+  var bodyParser = require('body-parser');
+
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
+
+  server.set('view engine', 'twig');
+  server.use(express.static('public'));
+
+  /*--
+  @route: /
+  @method: GET
+  Default page request to display the available configuration.
+  --*/
+  server.get('/', function(req, res) {
+    res.render('index', {
+      conf: null,
+      directories: glob.sync('./conf/*/**.js').map(function(val) { return val.replace('./conf/', '').replace('.js', ''); })
+    });
+  });
+
+  /*--
+  @route: /
+  @method: POST
+  Handle a post request to fetch scraper configurations and perfrom an initial
+  scrape to display data.
+  --*/
+  server.post('/', function(req, res) {
+    var conf = require('./conf/' + req.body.conf)(App);
+    App.scrape(conf, function(opts, result) {
+      res.render('index', {
+        conf: req.body.conf,
+        results: result,
+        directories: glob.sync('./conf/*/**.js').map(function(val) { return val.replace('./conf/', '').replace('.js', ''); })
+      });
+    });
+  });
+
+  /*--
+  @route: /download
+  @method: POST
+  Handle a CSV download request. This will generate a CSV and direct the output
+  to the browser ready for download.
+  --*/
+  server.post('/download', function(req, res) {
+    var data = JSON.parse(req.body.data);
+    var conf = require('./conf/test')(App);
+    var fields = conf.fields ? conf.fields : (typeof data.length == 'undefined' ? Object.keys(data) : Object.keys(data[0]));
+
+    json2csv({data: data, fields: fields}, function(err, csv) {
+      res.setHeader('Content-disposition', 'attachment; filename=data.csv');
+      res.set('Content-Type', 'text/csv');
+      res.status(200).send(csv);
+    });
+  });
+
+  server.get('/test', function(req, res) {
+    res.render('test');
+  });
+
+  server.listen(3333, function() {
+    console.log('Server started on port 3333');
+  });
+} else {
+  App.run(argv);
+}
---
+++ public/script.js
@@ -0,0 +1,50 @@
+/**
+ * @file
+ *
+ */
+(function($, Quill, Data) {
+
+  'use strict';
+
+  $(function() {
+
+    $('.item-row').each(function() {
+      let $this = $(this);
+      let $editor = $this.find('.value');
+      let $toolbar = $this.find('.toolbar');
+
+      if (!$editor.prop('id')) {
+        return;
+      }
+
+      $editor.data('editor', new Quill('#' + $editor.prop('id'), {theme: 'snow'}));
+
+      if ($toolbar.prop('id')) {
+        $editor.data('editor').addModule('toolbar', { container: '#' + $toolbar.prop('id') });
+      }
+    });
+
+    $('.btn-download').on('click', function() {
+      let data = [];
+      // Reconstruct the data object so we can push it to a CSV.
+      $('.item').each(function() {
+        let item = {};
+        $(this).find('.item-row').each(function() {
+          let $title = $(this).find('.lead');
+          let $value = $(this).find('.value');
+
+          if ($value.data('editor').getModule('toolbar')) {
+            item[$title.text()] = $value.data('editor').getHTML().replace(/(<div>)/g, '<p>').replace(/(<\/div>)/g, '</p>')
+          } else {
+            item[$title.text()] = $value.data('editor').getText().trim();
+          }
+        });
+        data.push(item);
+      });
+
+      $('form[name=download]').find('[name=data]').val(JSON.stringify(data)).end().submit();
+    });
+
+  });
+
+})(jQuery, Quill, Data);
